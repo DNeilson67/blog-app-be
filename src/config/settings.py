c833edb4e66826784829167324a5884e01c5fa23
DNeilson67 <davin.neilson@binus.ac.id>
2025-12-31 21:48:25 +0700

    feat(talent-growth): Update Frontend URL for deployment

--- src/config/settings.py
+++ src/config/settings.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
     
     # CORS
-    FRONTEND_URL: str = "http://localhost:3000"
+    FRONTEND_URL: str = "https://dn-blog.vercel.app"
     
     class Config:
         env_file = ".env"
